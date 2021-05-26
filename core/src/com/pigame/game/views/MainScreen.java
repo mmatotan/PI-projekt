00a3f3388ff592c13cc83da840ccd257c1fe21a1
mmatotan <marko.matotan4@gmail.com>
2021-05-26 19:48:16 +0200

    Postavljen glavni lik, omoguceno kretanje, barijere postavljene samo na rubove mape. Closes #17, closes #10

--- core/src/com/pigame/game/views/MainScreen.java
+++ core/src/com/pigame/game/views/MainScreen.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -22,6 +24,10 @@ import com.pigame.game.player.Player;
 public class MainScreen implements Screen, InputProcessor{
 	
 	final int TILE_SIZE = 16;
+	final int MAP_SIZE_X = 40;
+	final int MAP_SIZE_Y = 60;
+	final int CAM_WIDTH_TILES = 21;
+	final int CAM_HEIGHT_TILES = 13;
 	
 	private Game parent;
 	private Stage stage;
@@ -31,6 +37,9 @@ public class MainScreen implements Screen, InputProcessor{
 	TiledMap tiledMap;
 	TiledMapRenderer tiledMapRenderer;
 	
+	SpriteBatch spriteBatch;
+	Sprite sprite;
+	Texture texture;
 	Player player;
 	ProgressBar HPBar;
 	ProgressBar ManaBar;
@@ -43,18 +52,28 @@ public class MainScreen implements Screen, InputProcessor{
 	
 	@Override
 	public void show() {
+		Gdx.input.setInputProcessor(this);
+				
+		tiledMap = new TmxMapLoader().load("maps/MapGame.tmx"); //Loading of the .tmx file
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap); //Loading the map as orthogonal(bird view)
 		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		cam = new OrthographicCamera();
-		cam.setToOrtho(false, w/2, h/2); //Setting the camera to be zoomed in, too zoomed out otherwise
-		cam.update();
-		tiledMap = new TmxMapLoader().load("maps/MapGame.tmx"); //Loading of the .tmx file
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap); //Loading the map as orthogonal(bird view)
-		Gdx.input.setInputProcessor(this);
+		int mapWidth = tiledMap.getProperties().get("width", Integer.class);
+		int mapHeight = tiledMap.getProperties().get("height", Integer.class);
 		
-		player = new Player(); //Init of player class
+		cam = new OrthographicCamera();
+		//Setting the camera to be zoomed in, too zoomed out otherwise
+		cam.setToOrtho(false, CAM_WIDTH_TILES * TILE_SIZE, CAM_HEIGHT_TILES * TILE_SIZE);
+		// Looks odd but the camera now displays 20 tiles in width, and 12 in height
+		cam.translate(mapWidth/4 * TILE_SIZE, 0); //Sets the camera to the middle of map(x axis)
+		cam.update();
+		
+		player = new Player(mapWidth/2 * TILE_SIZE, 6 * TILE_SIZE); //Init of player class in middle of screen
+		spriteBatch = new SpriteBatch();
+		texture = new Texture(Gdx.files.internal("dungeon_assets/frames/wizzard_m_idle_anim_f0.png"));
+		sprite = new Sprite(texture);
 		
 		//Classic stage implementation for HUD(HP and Mana display)
 		Table table = new Table();
@@ -76,11 +95,19 @@ public class MainScreen implements Screen, InputProcessor{
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+		Gdx.gl.glClearColor(1f, 0f, 0f, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		cam.update();
 		tiledMapRenderer.setView(cam);
 		tiledMapRenderer.render();
+		
+		spriteBatch.setProjectionMatrix(cam.combined);
+		spriteBatch.begin();
+		sprite.setPosition(player.getX(), player.getY());
+		sprite.draw(spriteBatch);
+		spriteBatch.end();
 		
 		//Change color to red if HP lower than 20
 		if(player.getHP() > 50) {
@@ -131,14 +158,30 @@ public class MainScreen implements Screen, InputProcessor{
 	@Override
 	public boolean keyDown(int keycode) {
 		//Movement, temporarily moves the map around, should move the sprite of the main character
-        if(keycode == Input.Keys.LEFT)
-            cam.translate(-TILE_SIZE,0);
-        if(keycode == Input.Keys.RIGHT)
-            cam.translate(TILE_SIZE,0);
-        if(keycode == Input.Keys.DOWN)
-            cam.translate(0,-TILE_SIZE);
-        if(keycode == Input.Keys.UP)
-            cam.translate(0,TILE_SIZE);
+        if(keycode == Input.Keys.LEFT) {
+        	if(player.getX() > 0) {
+        		player.setX(player.getX()-TILE_SIZE);
+        		cam.translate(-TILE_SIZE,0);
+        	}
+        }
+        if(keycode == Input.Keys.RIGHT) {
+        	if(player.getX() < (MAP_SIZE_X - 1)*TILE_SIZE) {
+        		cam.translate(TILE_SIZE,0);
+        		player.setX(player.getX()+TILE_SIZE);
+        	}
+        }
+        if(keycode == Input.Keys.DOWN) {
+        	if(player.getY() > 0) {
+        		cam.translate(0,-TILE_SIZE);
+        		player.setY(player.getY()-TILE_SIZE);
+        	}
+        }
+        if(keycode == Input.Keys.UP) {
+        	if(player.getY() < (MAP_SIZE_Y - 1)*TILE_SIZE) {
+        		cam.translate(0,TILE_SIZE);
+        		player.setY(player.getY()+TILE_SIZE);
+        	}
+        }
 		return false;
 	}
 	
@@ -151,7 +194,6 @@ public class MainScreen implements Screen, InputProcessor{
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
