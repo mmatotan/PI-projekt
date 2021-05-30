a2826f16b57c98ed6dec2b68b72a488a1cbf915b
Ivana Tus <ivana.tus.ri@gmail.com>
2021-05-30 15:37:51 +0200

    Implentacija pauze. Closes #12

--- core/src/com/pigame/game/views/MainScreen.java
+++ core/src/com/pigame/game/views/MainScreen.java
@@ -2,6 +2,7 @@ package com.pigame.game.views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,10 +12,14 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.pigame.game.*;
 import com.pigame.game.player.Player;
@@ -34,12 +39,16 @@ public class MainScreen implements Screen, InputProcessor{
 	Player player;
 	ProgressBar HPBar;
 	ProgressBar ManaBar;
-
+	Window pauseWindow;
+	
+	boolean isPaused = false;
+	
 	public MainScreen(Game game) {
 		parent = game;
 		
 		stage = new Stage(new ScreenViewport());
 	}	
+	
 	
 	@Override
 	public void show() {
@@ -64,6 +73,20 @@ public class MainScreen implements Screen, InputProcessor{
 		
 		Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 		
+		// Pause window
+		pauseWindow = new Window("PAUSE", skin);
+		TextButton continueButton = new TextButton("Continue", skin);
+		continueButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				resume();
+			}
+		});
+		pauseWindow.padTop(64);
+		pauseWindow.add(continueButton);
+		pauseWindow.setSize(stage.getWidth() / 1.5f, stage.getHeight() / 1.5f);
+		pauseWindow.setPosition(stage.getWidth() / 2 - pauseWindow.getWidth() / 2, stage.getHeight() / 2 - pauseWindow.getHeight() / 2);
+		
 		HPBar = new ProgressBar(0, player.getMaxHP(), 1, false, skin);
 		ManaBar = new ProgressBar(0, player.getMaxMana(), 1, false, skin);
 		ManaBar.setColor(0, 0, 1, 1); //Blue
@@ -76,19 +99,34 @@ public class MainScreen implements Screen, InputProcessor{
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		cam.update();
-		tiledMapRenderer.setView(cam);
-		tiledMapRenderer.render();
-		
+		if(!isPaused) {
+			Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			cam.update();
+			tiledMapRenderer.setView(cam);
+			tiledMapRenderer.render();
+		} else {
+			//don't update, wait for resume
+			stage.addActor(pauseWindow);
+			if(Gdx.input.isKeyPressed(Keys.SPACE)) {
+				if(isPaused) {
+					resume();
+				}
+			}
+		}
+			
+		if(Gdx.input.isKeyPressed(Keys.SPACE)) {
+			if(!isPaused) {
+				pause();
+			}
+		}
 		//Change color to red if HP lower than 20
 		if(player.getHP() > 50) {
 			HPBar.setColor(0, 1, 0, 1);
 		} else {
 			HPBar.setColor(1, 0, 0, 1);
 		}
-		
+			
 		//Update values in the progress bars
 		HPBar.setValue(player.getHP());
 		ManaBar.setValue(player.getMana());
@@ -106,13 +144,12 @@ public class MainScreen implements Screen, InputProcessor{
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+		isPaused = true;
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
+		isPaused = false;
 		
 	}
 
